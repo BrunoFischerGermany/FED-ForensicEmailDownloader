38f52fd6dd0fa108777e345d77ad758350dbec0f
Bruno <Bruno.Fischer@zoll.bund.de>
2023-03-14 10:36:13 +0100

    Updated With menu. An Test IMAP Credentials

--- ForensicEmailDownloader.py
+++ ForensicEmailDownloader.py
@@ -9,8 +9,11 @@ from imapclient import  imap_utf7
 import logging
 import os
 import platform
+import pickle
 import ssl
 import subprocess
+import sys
+import time
 import urllib.request
 
 
@@ -28,7 +31,40 @@ def get_macos_version():
 
 def get_windows_version():
     return platform.win32_ver()[1]
+def delete_last_lines(n=1):
+    # Delete n lines
+    for _ in range(n):
+        # Move the cursor to the beginning of the line
+        sys.stdout.write('\033[F')
+        # Delete the line with spaces
+        sys.stdout.write('\033[K')
+def test_imap_credentials(imapurl, sslport, username, password):
+    ssl_context = ssl.create_default_context()
 
+    # don't check if certificate hostname doesn't match target hostname
+    ssl_context.check_hostname = False
+
+    # don't check if the certificate is trusted by a certificate authority
+    ssl_context.verify_mode = ssl.CERT_NONE
+    imap_server = imapclient.IMAPClient(host=imapurl, port=sslport, ssl_context=ssl_context)
+    try:
+        # Execute authentication
+        imap_server.login(username, password)
+        imapTestResult = 1
+        imapTestTimestamp = time.time()
+        return imapTestResult, imapTestTimestamp
+    except Exception as e:
+        imapTestResult = 0
+        imapTestTimestamp = time.time()
+        return imapTestResult, imapTestTimestamp
+
+    finally:
+        # Verbindung zum IMAP-Server schließen
+        imap_server.logout()
+
+#########################
+# global variables
+#########################
 programTitle = """
  ______ ______ _____         ______                       _      ______                 _ _ _____                      _                 _           
 |  ____|  ____|  __ \       |  ____|                     (_)    |  ____|               (_) |  __ \                    | |               | |          
@@ -37,78 +73,76 @@ programTitle = """
 | |    | |____| |__| |      | | | (_) | | |  __/ | | \__ \ | (__| |____| | | | | | (_| | | | |__| | (_) \ V  V /| | | | | (_) | (_| | (_| |  __/ |   
 |_|    |______|_____/       |_|  \___/|_|  \___|_| |_|___/_|\___|______|_| |_| |_|\__,_|_|_|_____/ \___/ \_/\_/ |_| |_|_|\___/ \__,_|\__,_|\___|_|    Version 0.1 
 
-(c) B. Fischer 2023"""
+(C) 2023 - BrunoFischerBerlin - MIT Licence
+Github: https://github.com/BrunoFischerGermany/FED-ForensicEmailDownloader
+Email: info @ bruno-fischer.de
+"""
 
 example1 = "python3 ForensicEmailDownloader.py --username user@example.com --password mypassword --imapurl imap.example.com --output C:\\tmp"
 example2 = "python3 ForensicEmailDownloader.py -u user@example.com -p mypassword -i imap.example.com -o /tmp/mails"
 
+#############################
+# Main Window
+#############################
 def main(output=None, username=None, password=None, imapurl=None, sslport=None, evidence=None, examiner=None, case=None):
+
+    imapTestTimestamp = None
+    imapTestResult = 0
     try:
         urllib.request.urlopen('https://www.google.com', timeout=5)
-        print('Internet connection exists.')
+        print("√ Internet connection exists.\n")
     except:
-        print('Internet connection does not exists.')
+        print("Internet connection does not exists. Please check the Internet connection. This program will now be closed.")
+        try:
+            input("Press enter to exit this program.")
+        except SyntaxError:
+            pass
         exit()
+    # Check an credential.file (credential.pkl) exists in the same folder of this program
+    pickle_file = "credential.pkl"
+    if os.path.exists(pickle_file) and os.access(pickle_file, os.W_OK):
+        print('√ Credential File found.\n')
+        with open(pickle_file, 'rb') as f:
+            var_list = pickle.load(f)
+        username, password, imapurl, sslport, output, examiner, case, evidence = var_list
+   # Check Args found. Show and Show them.
 
-    if args.output:
-        output = args.output
-    else:
-        output = input("Please enter the output path: ")
-    if not output:
-        output = "./"
+    # check Connection to IMAP Server. If False, then check IMAPUrl, SSLPort, username and password
     if args.username:
         username = args.username
-    else:
-        username = input("Please enter the username/email address: ")
+    if not username:
+        username = ""
     if args.password:
         password = args.password
-    else:
-        password = input("Please enter the password: ")
+    if not password:
+        password = ""
     if args.imapurl:
         imapurl = args.imapurl
-    else:
-        imapurl = input("Please enter the URL to the IMAP server: ")
+    if not imapurl:
+        imapurl = ""
     if args.sslport:
         sslport = args.sslport
-    else:
-        sslport = input(f"Please specify the port to the IMAP server (leave empty for default port {sslport}): ")
     if not sslport:
         sslport = 993
+    if args.output:
+        output = args.output
+    if not output:
+        output = "./"
     if args.examiner:
         examiner = args.examiner
-    else:
-        examiner = input("Please specify an Examiner: ")
+    if not examiner:
+        examiner = ""
     if args.case:
         case = args.case
-    else:
-        case = input("Please specify the case number: ")
+    if not case:
+        case = ""
     if args.evidence:
         evidence = args.evidence
-    else:
-        evidence = input("Please specify the evidence object number: ")
+    if not evidence:
+        evidence = ""
 
-    if os.path.exists(output) and os.access(output, os.W_OK):
-        print(f'The backup path "{output}" exists and is writeable.')
-        now = datetime.datetime.now()
-        folder_path = os.path.join(output, now.strftime("%Y-%m-%d_%H-%M-%S"))
-        # Ordner erstellen
-        os.makedirs(folder_path)
-        if os.path.exists(folder_path) and os.access(folder_path, os.W_OK):
-            print(f'The backup path "{folder_path}" exists and is writeable.')
-            output = folder_path
-        else:
-            print(f'The backup path "{folder_path}" could not be created.')
-            exit()
-    else:
-        print(f'The backup path "{output}" exists and is writable.')
-        exit()
-    logging_file = output + '/' + now.strftime("%Y-%m-%d_%H-%M-%S") + '.txt'
-    if os.path.exists(logging_file):
-        print(f'The log file already {logging_file} exists and will be overwritten.')
-    else:
-        print(f'The log file  {logging_file} will created.')
-    # leere Log-Datei erstellen
-    # Infos zum Betriebssystem
+    # Get System Data
+
     login_name = os.getlogin()
     system = platform.system()
 
@@ -124,80 +158,73 @@ def main(output=None, username=None, password=None, imapurl=None, sslport=None, 
     else:
         osSystem = "Other"
         osVersion = "Other"
-    startText = f"""{programTitle}
-case number: {case}
-evidence number: {evidence}
-examiner: {examiner}
 
-operating system: {osSystem}
-operating system version: {osVersion}
-computer user: {login_name}
+    ## Print out System data
+    print(f'operating system: {osSystem}')
+    print(f'operating system version: {osVersion}')
+    print(f'computer user: {login_name}\n')
 
-"""
+    ########################################
+    # Print out the defaults
+    ########################################
+    while True:
+        if imapTestTimestamp is not None:
+            imapTestString = "Result: " + time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(imapTestTimestamp))
+            if imapTestResult == 0:
+                imapTestString += " failed "
+            if imapTestResult == 1:
+                imapTestString += " successful "
+        else:
+            imapTestString = "Result: Credential not tested yet"
 
-    with open(logging_file, 'w') as f:
-        f.write(startText)
+        print(f'[1] Username/Email-Address:\t\t{username}')
+        print(f'[2] Password:\t\t\t\t{password}')
+        print(f'[3] IMAP-Server:\t\t\t{imapurl}')
+        print(f'[4] IMAP-Server-SSL-Port:\t\t{sslport}')
+        print(f'[5] Output-Path :\t\t\t{output}')
+        print(f'[6] Examiner:\t\t\t\t{examiner}')
+        print(f'[7] Case:\t\t\t\t{case}')
+        print(f'[8] Evicence:\t\t\t\t{evidence}')
+        print(f'[T] test imap credentials\t\t{imapTestString}')
+        print(f'[S] save email data to output path')
+        # 16 Lines
 
-    logging.basicConfig(filename=logging_file, level=logging.INFO,
-                        format='%(asctime)s - %(levelname)s - %(message)s')
+        choose = input('Please select an option [1-8; S; T] (q to exit): ')
+        # End loop when 'q' is entered
+        if choose == 'q':
+            break
+        if choose.upper()== '1':
+            username = input("Please enter the username/email address: ")
+        if choose.upper() == '2':
+            password = input("Please enter the password: ")
+        if choose.upper() == '3':
+            imapurl = input("Please enter the URL to the IMAP server: ")
+        if choose.upper() == '4':
+            sslport = input("Please specify the port to the IMAP server: ")
+        if choose.upper() == '5':
+            output = input("Please enter the output path: ")
+        if choose.upper() == '6':
+            examiner = input("Please specify an Examiner: ")
+        if choose.upper() == '7':
+            case = input("Please specify the case number: ")
+        if choose.upper() == '8':
+            evidence = input("Please specify the evidence object number: ")
+        if choose.upper() == "T":
+            # Test the Credentials
+            imapTestResult, imapTestTimestamp = test_imap_credentials(imapurl, sslport, username, password)
+        if choose.upper() != 'T' and choose.upper() != 'S':
+            with open(pickle_file, 'wb') as f:
+                pickle.dump([username, password, imapurl, sslport, output, examiner, case, evidence], f)
+            print('Credentical File updated')
+        else:
+            ## emty line for not shown credential file updated
+            print()
+        delete_last_lines(12)
 
-    logging.info('Internet connection exists.')
-    logging.info(f'The backup path "{folder_path}" exists and is writeable.')
-    ssl_context = ssl.create_default_context()
 
-    # don't check if certificate hostname doesn't match target hostname
-    ssl_context.check_hostname = False
-
-    # don't check if the certificate is trusted by a certificate authority
-    ssl_context.verify_mode = ssl.CERT_NONE
-    imap_server = imapclient.IMAPClient(host=imapurl, port=sslport, ssl_context=ssl_context)
-    try:
-        # Authentifizierung durchführen
-        imap_server.login(username, password)
-        email_folder_path = os.path.join(output, username)
-        os.makedirs(email_folder_path, exist_ok=True)
-        imap_server.select_folder('INBOX', readonly=True)
-        # IMAP-Operationen durchführen
-
-        folder_list = imap_server.list_folders()
-        for folder in folder_list:
-            # print(folder[2].decode())
-            decoded_folder = imap_utf7.decode(folder[2])
-            # decoded_folder = decoded_folder.split(' "/" ')[-1]
-            # Erstellen Sie die Ordnerstruktur im Zielverzeichnis
-            imap_sub_folder = os.path.join(email_folder_path, decoded_folder)
-            os.makedirs(imap_sub_folder, exist_ok=True)
-            logging.info(f"Folder {imap_sub_folder} created.")
-            # Wähle Ordner aus
-            encoded_folder = imap_utf7.encode(folder[2])
-            print(imap_utf7.encode(folder[2]))
-            imap_server.select_folder(imap_utf7.encode(folder[2]), readonly=True)
-            # status, email_ids = imap_server.search(None, "ALL")
-            # email_ids = email_ids[0].split()
-            # num_emails = len(email_ids)
-            # total_size = 0
-            # for email_id in email_ids:
-                #     status, email_data = imap_server.fetch(email_id, "(RFC822)")
-                #     email_message = email.message_from_bytes(email_data[0][1])
-                #     total_size += len(email_data[0][1])
-            # logging.info(f"Anzahl der E-Mailsim Ordner {folder[2]}: {num_emails} ")
-            # logging.info(f"Gesamtgröße der E-Mails im Ordner {folder[2]}: {total_size} Bytes")
-
-    except Exception as e:
-        logging.error(f"Error when using imapclient: {e}")
-        print(f"Error when using imapclient: {e} {encoded_folder}")
-
-    finally:
-        # Verbindung zum IMAP-Server schließen
-        imap_server.logout()
-
-    # Warte auf Ende
-    try:
-        input("Press enter to exit")
-    except SyntaxError:
-        pass
 
 if __name__ == '__main__':
+    os.system('mode con: cols=170 lines=40')
     parser = argparse.ArgumentParser(description={programTitle},
                                      formatter_class=argparse.RawDescriptionHelpFormatter,
                                      epilog="""Example:
@@ -207,9 +234,6 @@ if __name__ == '__main__':
 """)
     print(f"""
 {programTitle}
-
-Github: https://github.com/BrunoFischerGermany/FED-ForensicEmailDownloader
-Email: info @ bruno-fischer.de
 
 Example: 
 {example1}
